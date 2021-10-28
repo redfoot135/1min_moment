917f2d8be5d9e4395b9fe10fde7bd3969d61805a
aom414 <aom414@naver.com>
2021-10-28 13:56:47 +0900

    Feat:Comment-post-put-delete

--- server/app.js
+++ server/app.js
@@ -20,9 +20,9 @@ app.use(
 );
 
 //로그인 로그아웃 회원가입
-app.post("/signin", controllers.post_signin); //성근
+app.post("/signin", controllers.post_signin); //성근/완료
 app.post("/signup", controllers.post_signup); //재훈//완료
-app.post("/signout", controllers.post_signout); //성근
+app.post("/signout", controllers.post_signout); //성근/완료
 app.post("/socialSignin", controllers.post_socialSignin);//재훈
 //이메일 인증용도
 app.get("/emailauth", controllers.get_emailauth);//재훈//완료
@@ -43,14 +43,14 @@ app.put("/userinfo", controllers.put_userinfo); //재훈
 app.delete("/userinfo", controllers.delete_userinfo); //재훈
 
 //코멘트
-app.post("/comment", controllers.post_comment); //성근
-app.put("/comment", controllers.put_comment); //성근
-app.delete("/comment", controllers.delete_comment); //성근
+app.post("/comment", controllers.post_comment); //성근/완료
+app.put("/comment", controllers.put_comment); //성근/완료
+app.delete("/comment", controllers.delete_comment); //성근/완료
 
 //비디오 좋아요
-app.post("/like/video", controllers.post_like); //재훈
-app.get("/like/video", controllers.get_like);  //재훈
-app.delete("/like/video", controllers.delete_like);  //재훈
+app.post("/like/video", controllers.post_like); //성근
+app.get("/like/video", controllers.get_like);  //성근
+app.delete("/like/video", controllers.delete_like);  //성근
 
 //코멘트 좋아요
 app.post("/like/comment", controllers.post_like); //성근
