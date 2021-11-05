c119ee47c57263b503edb60605add727bbccf12f
Cho-Janghoon <dadasince2016@gmail.com>
2021-11-05 16:51:06 +0900

    fix myinfo in mypage/Myinfo.js

--- client/src/component/MyInfo.js
+++ client/src/component/MyInfo.js
@@ -2,14 +2,18 @@ import React from 'react'
 import "./MyInfo.css";
 
 const MyInfo = ({openMyInfoModalFunc}) => {
+
+    const 
     return (
         <div className="myInfo-modalbox">
             <div className="myInfo-modal">
               <span className="close" onClick={openMyInfoModalFunc}>
                   &times;
               </span>
-              <div>
-                  모달 만들어야 하는 페이지
+              <div className="myInfo-modal-content">
+                  <div className="myInfo-modal-title">내 정보</div>
+                  <button className="myInfo-modal-editmyinfo">내 정보 수정하기</button>
+                  <button className="myInfo-modal-secession">회원탈퇴</button>
               </div>                      
             </div>
         </div>
