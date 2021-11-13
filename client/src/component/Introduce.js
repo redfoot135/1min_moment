b09bf83900810bdc83e34ca82c597a89c42cd881
redfoot135 <tlswogns135@gmail.com>
2021-11-14 02:22:47 +0900

    Fix: Fix client CSS

--- client/src/component/Introduce.js
+++ client/src/component/Introduce.js
@@ -30,8 +30,8 @@ const Introduce = () => {
             <div class="carousel-item">
               <img src="https://i.ibb.co/GJwwpWP/006.png" class="d-block w-100 introduce" alt="..."/>
               <div className='carousel-caption introduce-search seach_input_box col-6'>
-                <input type='text' className="introduce-input search-input col-12" placeholder="검색 시작"/>
-                <img className="search-icon" src="https://i.ibb.co/FgWPvVM/Kakao-Talk-Photo-2021-11-12-13-30-49-removebg-preview.png" alt="소개글 검색 아이콘"/>
+                <input type='text' className="introduce-input col-12" placeholder="검색 시작"/>
+                <img className="introduce-search-icon" src="https://i.ibb.co/FgWPvVM/Kakao-Talk-Photo-2021-11-12-13-30-49-removebg-preview.png" alt="소개글 검색 아이콘"/>
               </div>
             </div>
           </div>
